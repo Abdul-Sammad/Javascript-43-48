bb9bd10e1f5d59a38850ed63fe409a2ccd2abf6f
Abdul-Sammad <100076834+Abdul-Sammad@users.noreply.github.com>
2022-05-30 01:13:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+
+//Question no 1 2
+//alert ("thanks for purchasing");
+//document.getElementsByTagName("img")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///question no5
+//var count = 0;
+//var element = document.getElementById('count');
+
+//function increment(){
+  //  count++;
+    //element.innerHTML = count;
+//}
+
+//function decrement(){
+  //  count--;
+    //element.innerHTML = count;
+//}
+
